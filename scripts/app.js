0de17b950c65c9cca879c5f6d67ae82f980accf6
BrockSpacek <brockspacek1@gmail.com>
2025-01-31 09:25:50 -0800

    Pushing Work

--- scripts/app.js
+++ scripts/app.js
@@ -33,8 +33,9 @@ let abilitiesOfPokemon = [];
 let movesOfPokemon = [];
 let typesOfPokemon = [];
 let locationOfPokemon = "";
-let imageOfPokemon;
 let evolutionsOfPokemon = [];
+let shinyImage = "";
+let dullImage = "";
 
 let isGenOneThroughFive = false;
 
@@ -51,50 +52,37 @@ Pokedex.addEventListener("keydown", async (event) => {
   isGenOneThroughFive = false;
 
   if (event.key === "Enter" && PokemonInput.value !== "") {
-    let data = await PokemonNameApi(PokemonInput.value);
-
-    if (data.id > 649) {
-      console.log("That pokemon is not in our Pokedex! Please try again");
-    } else {
-      isGenOneThroughFive = true;
-    }
-
-    if (isGenOneThroughFive == true) {
-      nameOfPokemon = data.name[0].toUpperCase() + data.name.slice(1);
-      idOfPokemon = `#${data.id}`;
-
-      data.types.forEach((types) => {
-        typesOfPokemon.push(types.type.name);
-      });
-
-      data.abilities.forEach((abilities) => {
-        abilitiesOfPokemon.push(abilities.ability.name);
-      });
-
-      data.moves.forEach((moves) => {
-        movesOfPokemon.push(moves.move.name);
-      });
-
-      PokemonName.innerText = nameOfPokemon;
-      PokemonId.innerText = idOfPokemon;
-      PokemonType.innerText = `Types: ${typesOfPokemon.join(" | ")}`;
-      PokemonAbilities.innerText = abilitiesOfPokemon.join(" | ");
-      PokemonMoves.innerText = movesOfPokemon.join(" | ");
-      PokemonImage.src = data.sprites.front_default;
-    }
+    PokeLogic(PokemonInput.value);
   }
 });
 
+shinyBtn.addEventListener("click", () => {
+  if (PokemonImage.src == dullImage) {
+    PokemonImage.src = shinyImage;
+  } else {
+    PokemonImage.src = dullImage;
+  }
+});
+
+async function PokeLogic(pokeSearch) {
+  let data = await PokemonNameApi(pokeSearch);
+
+  if (data.id > 649) {
+    console.log("That pokemon is not in our Pokedex! Please try again");
+  } else {
+    isGenOneThroughFive = true;
+  }
 
 
-/*  async function PokeLogic(){
-
-if (isGenOneThroughFive == true) {
+  if (isGenOneThroughFive == true) {
     nameOfPokemon = data.name[0].toUpperCase() + data.name.slice(1);
-    idOfPokemon = data.id;
+    idOfPokemon = `#${data.id}`;
 
     data.types.forEach((types) => {
-      typesOfPokemon.push(types.type.name);
+      types.type.name[0].toUpperCase() + types.type.name.slice(1);
+      typesOfPokemon.push(
+        types.type.name[0].toUpperCase() + types.type.name.slice(1)
+      );
     });
 
     data.abilities.forEach((abilities) => {
@@ -105,12 +93,16 @@ if (isGenOneThroughFive == true) {
       movesOfPokemon.push(moves.move.name);
     });
 
+    shinyImage = data.sprites.front_shiny;
+    dullImage = data.sprites.front_default;
+
     PokemonName.innerText = nameOfPokemon;
     PokemonId.innerText = idOfPokemon;
-    PokemonType.innerText = typesOfPokemon.join(" | ");
+    PokemonType.innerText = `Types: ${typesOfPokemon.join(" | ")}`;
     PokemonAbilities.innerText = abilitiesOfPokemon.join(" | ");
     PokemonMoves.innerText = movesOfPokemon.join(" | ");
-    PokemonImage.src = data.sprites.front_default;
+    PokemonImage.src = dullImage;
   }
 }
-  */
+
+PokeLogic("Bulbasaur");
